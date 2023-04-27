c9603575b2321932327c2ecc5c2c862905c8d164
v4nchous <v4nchous@gmail.com>
2023-04-27 23:26:25 +0300

    padding / body / image height

---
+++ munchkin_notebook/lib/pages/enter_code.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:munchkin_notebook/navigation/router.gr.dart';
+
+import 'package:munchkin_notebook/pages/basic_widgets/base_page.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/buttons.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/description.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/enter_code_group.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/title.dart';
+
+@RoutePage()
+class EnterCode extends StatelessWidget {
+  const EnterCode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MyBasePage(
+        title: MyTitle(text: AppLocalizations.of(context)!.enterCodeTitle),
+        body: Expanded(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              EnterCodeGroup(),
+              const SizedBox(height: 20),
+              Expanded(
+                child: MyDescription(
+                    text: AppLocalizations.of(context)!.enterCodeDescription),
+              ),
+            ],
+          ),
+        ),
+        actions: Column(
+          children: [
+            MyPrimaryButton(
+                text: AppLocalizations.of(context)!.enterCodeAction1),
+            const SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                AutoRouter.of(context).navigate(const CreateGame());
+              },
+              child: MySecondaryButton(
+                  text: AppLocalizations.of(context)!.enterCodeAction2),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ munchkin_notebook/lib/pages/self_counting.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:munchkin_notebook/navigation/router.gr.dart';
+
+import 'package:munchkin_notebook/pages/basic_widgets/base_page.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/buttons.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/description.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/image.dart';
+import 'package:munchkin_notebook/pages/basic_widgets/page_units/title.dart';
+
+@RoutePage()
+class SelfCounting extends StatelessWidget {
+  const SelfCounting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MyBasePage(
+        title: MyTitle(text: AppLocalizations.of(context)!.selfCountingTitle),
+        body: Expanded(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              MyImage(
+                  path: AppLocalizations.of(context)!.selfCountingImagePath),
+              const SizedBox(height: 20),
+              Expanded(
+                child: MyDescription(
+                    text:
+                        AppLocalizations.of(context)!.selfCountingDescription),
+              ),
+            ],
+          ),
+        ),
+        actions: Column(
+          children: [
+            MyPrimaryButton(
+                text: AppLocalizations.of(context)!.selfCountingAction1),
+            const SizedBox(height: 20),
+            MyPrimaryButton(
+                text: AppLocalizations.of(context)!.selfCountingAction2),
+            const SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                AutoRouter.of(context).navigate(const MaxLevel());
+              },
+              child: MySecondaryButton(
+                  text: AppLocalizations.of(context)!.selfCountingAction3),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
