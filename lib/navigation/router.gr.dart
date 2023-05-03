4d4087cb0d7c1c1f8aa8e2ca1f5f463a8c39c239
v4nchous <v4nchous@gmail.com>
2023-05-03 14:30:50 +0300

    router

--- lib/navigation/router.gr.dart
+++ lib/navigation/router.gr.dart
@@ -9,33 +9,33 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i6;
-import 'package:munchkin_notebook/pages/create_game.dart' as _i2;
-import 'package:munchkin_notebook/pages/enter_code.dart' as _i5;
+import 'package:munchkin_notebook/pages/create_game.dart' as _i3;
+import 'package:munchkin_notebook/pages/enter_code.dart' as _i1;
 import 'package:munchkin_notebook/pages/join_game.dart' as _i4;
-import 'package:munchkin_notebook/pages/max_level.dart' as _i1;
-import 'package:munchkin_notebook/pages/self_counting.dart' as _i3;
+import 'package:munchkin_notebook/pages/max_level.dart' as _i2;
+import 'package:munchkin_notebook/pages/self_counting.dart' as _i5;
 
 abstract class $AppRouter extends _i6.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
   final Map<String, _i6.PageFactory> pagesMap = {
+    EnterCode.name: (routeData) {
+      return _i6.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i1.EnterCode(),
+      );
+    },
     MaxLevel.name: (routeData) {
       return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i1.MaxLevel(),
+        child: const _i2.MaxLevel(),
       );
     },
     CreateGame.name: (routeData) {
       return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i2.CreateGame(),
-      );
-    },
-    SelfCounting.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
-        routeData: routeData,
-        child: const _i3.SelfCounting(),
+        child: const _i3.CreateGame(),
       );
     },
     JoinGame.name: (routeData) {
@@ -44,17 +44,31 @@ abstract class $AppRouter extends _i6.RootStackRouter {
         child: const _i4.JoinGame(),
       );
     },
-    EnterCode.name: (routeData) {
+    SelfCounting.name: (routeData) {
       return _i6.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i5.EnterCode(),
+        child: const _i5.SelfCounting(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.MaxLevel]
+/// [_i1.EnterCode]
+class EnterCode extends _i6.PageRouteInfo<void> {
+  const EnterCode({List<_i6.PageRouteInfo>? children})
+      : super(
+          EnterCode.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'EnterCode';
+
+  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i2.MaxLevel]
 class MaxLevel extends _i6.PageRouteInfo<void> {
   const MaxLevel({List<_i6.PageRouteInfo>? children})
       : super(
@@ -68,7 +82,7 @@ class MaxLevel extends _i6.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i2.CreateGame]
+/// [_i3.CreateGame]
 class CreateGame extends _i6.PageRouteInfo<void> {
   const CreateGame({List<_i6.PageRouteInfo>? children})
       : super(
@@ -77,20 +91,6 @@ class CreateGame extends _i6.PageRouteInfo<void> {
         );
 
   static const String name = 'CreateGame';
-
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
-}
-
-/// generated route for
-/// [_i3.SelfCounting]
-class SelfCounting extends _i6.PageRouteInfo<void> {
-  const SelfCounting({List<_i6.PageRouteInfo>? children})
-      : super(
-          SelfCounting.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'SelfCounting';
 
   static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
 }
@@ -110,15 +110,15 @@ class JoinGame extends _i6.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i5.EnterCode]
-class EnterCode extends _i6.PageRouteInfo<void> {
-  const EnterCode({List<_i6.PageRouteInfo>? children})
+/// [_i5.SelfCounting]
+class SelfCounting extends _i6.PageRouteInfo<void> {
+  const SelfCounting({List<_i6.PageRouteInfo>? children})
       : super(
-          EnterCode.name,
+          SelfCounting.name,
           initialChildren: children,
         );
 
-  static const String name = 'EnterCode';
+  static const String name = 'SelfCounting';
 
   static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
 }
