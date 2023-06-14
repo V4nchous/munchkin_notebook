fc9a2d134ac3173c0c4b8c242100cce14eb81506
V4nchous <v4nchous@gmail.com>
2023-06-14 11:36:44 +0300

    "game" in game_bloc -> private

--- lib/features/game/presentation/bloc/game_bloc.dart
+++ lib/features/game/presentation/bloc/game_bloc.dart
@@ -10,43 +10,43 @@ part 'game_event.dart';
 part 'game_state.dart';
 
 class GameBloc extends Bloc<CreateGameEvent, CreateGameState> {
-  Game? game;
+  Game? _game;
 
   GameBloc() : super(GameNotReady()) {
     on<CreateGameEvent>((event, emit) {
       if (event is StartGame) {
-        game = Game(
+        _game = Game(
             maxLevel: event.maxLevel,
             isGameMaster: event.isGameMaster,
             gameCode: Random().nextInt(100000),
             players: []);
 
-        emit(GameCreated(game!));
+        emit(GameCreated(_game!));
       }
       if (event is ChangeGameMaxLevel) {
-        if (game != null) {
-          game?.maxLevel = event.maxLevel;
-          emit(GameCreated(game!));
+        if (_game != null) {
+          _game?.maxLevel = event.maxLevel;
+          emit(GameCreated(_game!));
         }
       }
       if (event is AddPlayer) {
-        game!.players.add(event.player);
+        _game!.players.add(event.player);
 
-        emit(GameCreated(game!));
+        emit(GameCreated(_game!));
       }
       if (event is DecrementPlayerLevel) {
         if (event.player.level > 1) {
           event.player.level--;
         }
 
-        emit(GameCreated(game!));
+        emit(GameCreated(_game!));
       }
       if (event is IncrementPlayerLevel) {
-        if (event.player.level < game!.maxLevel) {
+        if (event.player.level < _game!.maxLevel) {
           event.player.level++;
         }
 
-        emit(GameCreated(game!));
+        emit(GameCreated(_game!));
       }
 
       if (event is DecrementPlayerBonus) {
@@ -54,12 +54,12 @@ class GameBloc extends Bloc<CreateGameEvent, CreateGameState> {
           event.player.bonus--;
         }
 
-        emit(GameCreated(game!));
+        emit(GameCreated(_game!));
       }
       if (event is IncrementPlayerBonus) {
         event.player.bonus++;
 
-        emit(GameCreated(game!));
+        emit(GameCreated(_game!));
       }
     });
   }
