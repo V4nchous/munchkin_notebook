d498d37f84746448f47861aaa8aa30e48989bf65
V4nchous <v4nchous@gmail.com>
2023-07-11 12:52:21 +0300

    Hide statusbar after app open

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app.dart';
 
 void main() {
   const app = App();
   runApp(app);
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [],
+  );
 }
