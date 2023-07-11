0030adae6800572d62ba0ecf29dd50afe5a19ff2
V4nchous <v4nchous@gmail.com>
2023-07-11 23:14:59 +0300

    fix

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class L10n {
-  static final all = [
-    const Locale('ru'),
-    const Locale('en'),
+  static const all = [
+    Locale('ru'),
+    Locale('en'),
   ];
 }
